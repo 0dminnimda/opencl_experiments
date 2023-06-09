ff65a7e435965a09273fff6f15e9ec978b2cf6ee
0dminnimda <0dminnimda@gmail.com>
2023-06-09 21:41:03 +0300

    gpu\test4.cpp: try to make shared memory

--- gpu/test4.cpp
+++ gpu/test4.cpp
@@ -10,28 +10,40 @@
 
 
 static const std::string kernel_source = R"(
-__kernel void vector_add(__global const float *A,
-                         __global const float *B,
+__kernel void vector_add(__global float *A,
+                         __global float *B,
                          __global float *C) {
     int gid = get_global_id(0);   
     C[gid] = A[gid] + B[gid];
     printf("%p %p\n", A, B);
+    A[gid] = 1;
+    B[gid] = 1;
 }
 )";
 
-int main() {
-    std::vector<float> A = {1.0f, 2.0f, 3.0f, 4.0f, 5.0f};
-    std::vector<float> B = {5.5f, 4.0f, 3.0f, 2.3f, 1.0f};
-    std::vector<float> C(A.size());
-
+int main() {    
     cl::Device device;
     if (get_gpu_device(device)) { return -1; }
     cl::Context context(device);
     cl::CommandQueue queue(context, device);
 
-    cl::Buffer bufferA(context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR,
+    std::vector<float> A = {1.0f, 2.0f, 3.0f, 4.0f, 5.0f};
+    std::vector<float> B = {5.5f, 4.0f, 3.0f, 2.3f, 1.0f};
+    std::vector<float> C(A.size());
+
+    for (size_t i = 0; i < C.size(); ++i) {
+        std::cout << "A[" << i << "] = " << A[i] << std::endl;
+    }
+    for (size_t i = 0; i < C.size(); ++i) {
+        std::cout << "B[" << i << "] = " << B[i] << std::endl;
+    }
+    for (size_t i = 0; i < C.size(); ++i) {
+        std::cout << "C[" << i << "] = " << C[i] << std::endl;
+    }
+
+    cl::Buffer bufferA(context, CL_MEM_READ_WRITE | CL_MEM_USE_HOST_PTR,
                        sizeof(float) * A.size(), A.data());
-    cl::Buffer bufferB(context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR,
+    cl::Buffer bufferB(context, CL_MEM_READ_WRITE | CL_MEM_USE_HOST_PTR,
                        sizeof(float) * B.size(), B.data());
     cl::Buffer bufferC(context, CL_MEM_WRITE_ONLY, sizeof(float) * C.size());
 
@@ -72,10 +84,18 @@ int main() {
     cl::NDRange localSize(1);
     queue.enqueueNDRangeKernel(kernel, cl::NullRange, globalSize, localSize);
 
+    queue.finish();
+
     // Read results from the output buffer
     queue.enqueueReadBuffer(bufferC, CL_TRUE, 0, sizeof(float) * C.size(), C.data());
 
     // Print the result
+    for (size_t i = 0; i < C.size(); ++i) {
+        std::cout << "A[" << i << "] = " << A[i] << std::endl;
+    }
+    for (size_t i = 0; i < C.size(); ++i) {
+        std::cout << "B[" << i << "] = " << B[i] << std::endl;
+    }
     for (size_t i = 0; i < C.size(); ++i) {
         std::cout << "C[" << i << "] = " << C[i] << std::endl;
     }
