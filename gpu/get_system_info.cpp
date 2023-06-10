9c23568d0e883389fb9166920479cd39454c82cc
0dminnimda <0dminnimda@gmail.com>
2023-06-10 03:02:50 +0300

    gpu\get_system_info.cpp: add device extensions as `-v`

--- gpu/get_system_info.cpp
+++ gpu/get_system_info.cpp
@@ -6,17 +6,41 @@
 
 #include "opencl_helpers.cpp"
 
-int main() {
+int main(int argc, char *argv[]) {
     std::vector<cl::Platform> platforms;
     cl::Platform::get(&platforms);
 
+    bool verbose = (argc > 1 && std::string(argv[1]) == "-v");
+
+    std::string ident = "    ";
     for (auto &platform : platforms) {
         std::cout << "Found platform: " << platform << " with devices:" << std::endl;
 
         std::vector<cl::Device> devices;
         platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
 
-        for (auto &device : devices) { std::cout << "    " << device << std::endl; }
+        for (auto &device : devices) {
+            std::cout << ident << device;
+            if (verbose) std::cout << " with extensions: ";
+            std::cout << std::endl;
+
+            if (!verbose) continue;
+            std::vector<std::string> extensions = get_device_extensions(device);
+            for (auto &extension : extensions) {
+                std::cout << ident << ident << extension << std::endl;
+            }
+
+            std::cout << std::endl;
+            if (std::find(extensions.begin(), extensions.end(), "cl_khr_il_program") !=
+                extensions.end()) {
+                std::cout << ident << ident
+                          << "cl_khr_il_program extension is supported\n";
+            } else {
+                std::cout << ident << ident
+                          << "cl_khr_il_program extension is not supported\n";
+            }
+            std::cout << std::endl;
+        }
 
         std::cout << std::endl;
     }
